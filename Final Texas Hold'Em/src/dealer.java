10a6951a65b54924d42e1ec8e98ca4eb899e39db
Madison.Golesh <Madison.Golesh@MU-C3-12.mullen.pvt.k12.co.us>
2015-02-11 15:52:52 -0700

    last draft

--- Final Texas Hold'Em/src/dealer.java
+++ Final Texas Hold'Em/src/dealer.java
@@ -1,27 +1,40 @@
 import java.util.Scanner;
 public class dealer
 	{
+	public static String [] dealerFace1={"Diamonds","Clubs", "Spades", "Hearts"};
+	public static String [] dealerNumber1={"2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King","Ace"};
+	public static String [] dealerFace2={"Diamonds","Clubs", "Spades", "Hearts"};
+	public static String [] dealerNumber2={"2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King","Ace"};
+	public static String [] dealerFace3={"Diamonds","Clubs", "Spades", "Hearts"};
+	public static String [] dealerNumber3={"2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King","Ace"};
+	public static String [] dealerFace4={"Diamonds","Clubs", "Spades", "Hearts"};
+	public static String [] dealerNumber4={"2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King","Ace"};
+	public static String [] dealerFace5={"Diamonds","Clubs", "Spades", "Hearts"};
+	public static String [] dealerNumber5={"2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King","Ace"};
+	static int dieRoll;
+	static int dieRoll1;
+	static int dieRoll2;
+	static int dieRoll3;
+	static int dieRoll4;
+	static int dieRoll5;
+	static int dieRoll6;
+	static int dieRoll7;
+	static int dieRoll8;
+	static int dieRoll9;	
 	public static void dealerFirst()
 		{
 		Scanner userInput=new Scanner(System.in);
-		String [] dealerFace1={"Diamonds","Clubs", "Spades", "Hearts"};
-		int dieRoll=(int)(Math.random()*dealerFace1.length);
-		String [] dealerNumber1={"Ace","2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King"};
-		int dieRoll2=(int)(Math.random()*dealerNumber1.length);
-		
+		dieRoll=(int)(Math.random()*dealerFace1.length);
+		dieRoll2=(int)(Math.random()*dealerNumber1.length);
 		
 		Scanner userInput1=new Scanner(System.in);
-		String [] dealerFace2={"Diamonds","Clubs", "Spades", "Hearts"};
-		int dieRoll1=(int)(Math.random()*dealerFace2.length);
-		String [] dealerNumber2={"Ace","2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King"};
-		int dieRoll3=(int)(Math.random()*dealerNumber2.length);
+		dieRoll1=(int)(Math.random()*dealerFace2.length);
+		dieRoll3=(int)(Math.random()*dealerNumber2.length);
 		
 		
 		Scanner userInput2=new Scanner(System.in);
-		String [] dealerFace3={"Diamonds","Clubs", "Spades", "Hearts"};
-		int dieRoll4=(int)(Math.random()*dealerFace3.length);
-		String [] dealerNumber3={"Ace","2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King"};
-		int dieRoll5=(int)(Math.random()*dealerNumber3.length);
+		dieRoll4=(int)(Math.random()*dealerFace3.length);
+		dieRoll5=(int)(Math.random()*dealerNumber3.length);
 			{
 			System.out.println("Dealer: These are your first cards to work with.");
 			System.out.println(""+dealerNumber1[dieRoll2] +" of "+dealerFace1[dieRoll]+"     " + dealerNumber2[dieRoll3]+ " of " + dealerFace2[dieRoll1]+"     " + dealerNumber3[dieRoll5]+ " of "+ dealerFace3[dieRoll4]);
@@ -32,23 +45,19 @@ public class dealer
 		public static void dealer2()
 		{
 		Scanner userInput3=new Scanner(System.in);
-		String [] dealerFace4={"Diamonds","Clubs", "Spades", "Hearts"};
-		int dieRoll6=(int)(Math.random()*dealerFace4.length);
-		String [] dealerNumber4={"Ace","2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King"};
-		int dieRoll7=(int)(Math.random()*dealerNumber4.length);
+		dieRoll6=(int)(Math.random()*dealerFace4.length);
+		dieRoll7=(int)(Math.random()*dealerNumber4.length);
 			{
 			System.out.println("Here is your 4th card.");
-			System.out.println(""+dealerNumber4[dieRoll7]+" of "+ dealerFace4[dieRoll6]+ " ");
+			System.out.println(""+dealerNumber5+dealerNumber4[dieRoll7]+" of "+ dealerFace4[dieRoll6]+ " ");
 			}
 		}
 			
 		public static void dealer3()
 		{
 		Scanner userInput4=new Scanner(System.in);
-		String [] dealerFace5={"Diamonds","Clubs", "Spades", "Hearts"};
-		int dieRoll8=(int)(Math.random()*dealerFace5.length);
-		String [] dealerNumber5={"Ace","2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King"};
-		int dieRoll9=(int)(Math.random()*dealerNumber5.length);
+		dieRoll8=(int)(Math.random()*dealerFace5.length);
+		dieRoll9=(int)(Math.random()*dealerNumber5.length);
 			{
 			System.out.println("Here is your 5th card and final card.");
 			System.out.println(""+dealerNumber5[dieRoll9]+" of "+ dealerFace5[dieRoll8]+ " ");
--- Final Texas Hold'Em/src/gameRunner.java
+++ Final Texas Hold'Em/src/gameRunner.java
@@ -1,4 +1,6 @@
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class gameRunner
 {
 	public static void main(String[] args)
@@ -13,71 +15,102 @@ public class gameRunner
 			betting.bettingFourth();
 			checkForWon();
 		}
+	public static String[] randomFace1={"Diamonds","Clubs", "Spades", "Hearts"};
+	public static String[] randomFace2={"Diamonds","Clubs", "Spades", "Hearts"};
+	public static String [] randomNumber1={"2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King","Ace"};
+	public static String [] randomNumber2={"2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King","Ace"};
+	public static int dieRoll;
+	public static int dieRoll3;
+	static int dieRoll22;
+	static int dieRoll33;
+	static int dieRoll44;
+	public static boolean isAStraight=false;
 	public static void greetUser()
-	{
-		Scanner userInput=new Scanner(System.in);
-		System.out.println("Lets play a game of Texas Hold'Em!");
-		System.out.println("What is your name?");
-		String name=userInput.nextLine();
-		String [] randomCompliment={"Looking good today","You are so smart","I have a feeling you are going to win this game","Your outfit looks very nice, and super original","You are so much smarter than the cumputer you are definetly going to win"};
-		int dieRoll1=(int)(Math.random()*randomCompliment.length);
-		System.out.println("Hi there " + name + "! " + randomCompliment[dieRoll1] + "!");
-		System.out.println("Are you ready to play a game of Texas Hold'Em?");
-		System.out.println("(1) Yes");
-		System.out.println("(2) No");
-		int choice=userInput.nextInt();
-			if (choice==1)
+		{
+			Scanner userInput=new Scanner(System.in);
+			System.out.println("Lets play a game of Texas Hold'Em!");
+			System.out.println("What is your name?");
+			String name=userInput.nextLine();
+			String [] randomCompliment={"Looking good today","You are so smart","I have a feeling you are going to win this game","Your outfit looks very nice, and super original","You are so much smarter than the cumputer you are definetly going to win"};
+			int dieRoll1=(int)(Math.random()*randomCompliment.length);
+			System.out.println("Hi there " + name + "! " + randomCompliment[dieRoll1] + "!");
+			System.out.println("Are you ready to play a game of Texas Hold'Em?");
+			System.out.println("(1) Yes");
+			System.out.println("(2) No");
+			int choice=userInput.nextInt();
+				if (choice==1)
+					{
+					System.out.println("Good lets play!");
+					}
+				else if (choice==2)
+					{
+					System.out.println("To bad I am forcing you to play anyways!");
+					}
+				else
+					{
+					System.out.println("Hahaha nice try you still have to play the game!");
+					}
+			System.out.println("Do you know the rules for this poker game?");
+			System.out.println("(1) Yes");
+			System.out.println("(2) No");
+			int choice2=userInput.nextInt();
+				if (choice2==1)
+					{
+					System.out.println("Ok awesome lets start the game!");
+					}
+				else if (choice2==2)
+					{
+					System.out.println("Ok so lets go over the rules: You will be given two cards that you may look at throughout the game. You will be allowed to bet after you get your two cards, after you see each new dealed card, and then again at the end. Your goal is to use your cards and the dealed cards to create a winning hand. ");
+					}
+				else 
+					{
+					System.out.println("You think your so smart so I am just going to assume you know how to play the game. So lets start!");
+					}
+				Scanner userInput1=new Scanner(System.in);
+				dieRoll=(int)(Math.random()*randomFace1.length);
+				dieRoll22=(int)(Math.random()*randomNumber1.length);
+					{
+				Scanner userInput2=new Scanner(System.in);
+				dieRoll33=(int)(Math.random()*randomFace2.length);
+				dieRoll44=(int)(Math.random()*randomNumber2.length);
+				
+						{
+						System.out.println("Your two cards: "+ randomNumber1[dieRoll22] + " of " + randomFace1[dieRoll] + "     " + randomNumber2[dieRoll44] + " of " + randomFace2[dieRoll3] +".");
+						}
+					}
+		}
+
+		public static void checkForWon()
+			{
+		ArrayList cardsDealt=new ArrayList();
+		cardsDealt.add(randomNumber1[dieRoll22]);
+		cardsDealt.add(randomNumber2[dieRoll44]);
+		cardsDealt.add(dealer.dealerNumber1[dealer.dieRoll2]);
+		cardsDealt.add(dealer.dealerNumber2[dealer.dieRoll3]);
+		cardsDealt.add(dealer.dealerNumber3[dealer.dieRoll5]);
+		cardsDealt.add(dealer.dealerNumber4[dealer.dieRoll7]);
+		cardsDealt.add(dealer.dealerNumber5[dealer.dieRoll9]);
+		Collections.sort(cardsDealt);
+			
+		if ((dieRoll==dieRoll33)&&(dieRoll33==(dealer.dieRoll))&&((dealer.dieRoll)==(dealer.dieRoll1))&&((dealer.dieRoll1)==(dealer.dieRoll4))&&((dealer.dieRoll4)==(dealer.dieRoll6))&&((dealer.dieRoll6)==(dealer.dieRoll8)))
 				{
-				System.out.println("Good lets play!");
+				System.out.println("You got a Flush!");	
 				}
-			else if (choice==2)
+		for(int i=0;i<4;i++)
+			{
+			if (cardsDealt.get(i+1)==cardsDealt.get(i))
 				{
-				System.out.println("To bad I am forcing you to play anyways!");
+				isAStraight=true;
+				System.out.println("You got a Straight!");
 				}
 			else
-				{
-				System.out.println("Hahaha nice try you still have to play the game!");
-				}
-		System.out.println("Do you know the rules for this poker game?");
-		System.out.println("(1) Yes");
-		System.out.println("(2) No");
-		int choice2=userInput.nextInt();
-			if (choice2==1)
-				{
-				System.out.println("Ok awesome lets start the game!");
-				}
-			else if (choice2==2)
-				{
-				System.out.println("Ok so lets go over the rules: You will be given two cards that you may look at throughout the game. You will be allowed to bet after you get your two cards, after you see each new dealed card, and then again at the end. Your goal is to use your cards and the dealed cards to create a winning hand. ");
-				}
-			else 
-				{
-				System.out.println("You think your so smart so I am just going to assume you know how to play the game. So lets start!");
-				}
-		Scanner userInput1=new Scanner(System.in);
-		String [] randomFace1={"Diamonds","Clubs", "Spades", "Hearts"};
-		int dieRoll=(int)(Math.random()*randomFace1.length);
-		String [] randomNumber1={"Ace","2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King"};
-		int dieRoll2=(int)(Math.random()*randomNumber1.length);
 			{
-		Scanner userInput2=new Scanner(System.in);
-		String [] randomFace2={"Diamonds","Clubs", "Spades", "Hearts"};
-		int dieRoll3=(int)(Math.random()*randomFace2.length);
-		String [] randomNumber2={"Ace","2","3","4","5","6", "7", "8", "9","10", "Jack", "Queen", "King"};
-		int dieRoll4=(int)(Math.random()*randomNumber2.length);
-		
-				{
-				System.out.println("Your two cards: "+ randomNumber1[dieRoll2] + " of " + randomFace1[dieRoll] + "     " + randomNumber2[dieRoll4] + " of " + randomFace2[dieRoll3] +".");
-				}
+			isAStraight=false;
 			}
-		}
-		public static void checkForWon()
-		{
-		
-		
-		
-		}
+		}			
+			}
 }
+
 
 
 
